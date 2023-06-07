ff8bc8e2376af3d1261300bd722c9864f1e73a79
rend1027 <rend1027@yahoo.com>
2023-06-07 09:42:05 -0400

    update page title

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,7 +2,9 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-
+interface Props {
+  onSearch: (searchText: string) => void;
+}
 const SearchInput = ({ onSearch }: Props) => {
   const ref = useRef<HTMLInputElement>(null);
 
